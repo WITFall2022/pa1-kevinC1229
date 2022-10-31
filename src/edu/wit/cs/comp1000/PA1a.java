1d63cb6c0f30a9554a130d5dd59da6911333b571
430063 <430063@664-161455-lab.schools.mybps.org>
2022-10-31 10:26:06 -0400

    I completed my PA1 part a and b.

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,31 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		Scanner input = new Scanner(System.in); 
+		
+		int yards;
+		System.out.printf("Enter number of yards: ");
+		yards = input.nextInt();
+		
+		int feet;
+		System.out.printf("Enter number of feet:");
+		feet = input.nextInt();
+		
+		int inches;
+		System.out.printf("Enter number of inches:");
+		inches = input.nextInt();
+		
+		System.out.printf("Total number of inches: ");
+		int ans;
+		ans = feet*12+yards*36+inches;
+		System.out.print(ans);
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA1b.java
+++ src/edu/wit/cs/comp1000/PA1b.java
@@ -1,11 +1,38 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+	Scanner input = new Scanner(System.in);
+		
+		int Totalinches;
+		
+		System.out.printf("Enter Number of Inches: ");
+		
+		Totalinches = input.nextInt();
+		
+		int yards;
+		
+		yards = Totalinches/36;
+		
+		System.out.printf("yards:%d%n", yards);
+		
+		int feet;
+		
+		feet = Totalinches %36/12;
+		
+		System.out.printf("feet:%d%n", feet);
+		
+		int inches;
+		
+		inches = Totalinches%36%12/1;
+		
+		System.out.printf("inches: %d%n",inches);
 	}
 
 }
